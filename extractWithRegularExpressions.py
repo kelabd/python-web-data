8747b874b177af3ac500759c4937b434dd55ea66
kelabd <karam.elabd@gmail.com>
2023-10-27 14:43:34 -0700

    clean-up

--- extractWithRegularExpressions.py
+++ extractWithRegularExpressions.py
@@ -5,3 +5,21 @@ Created on Fri Oct 27 14:27:46 2023
 @author: karam
 """
 
+import re
+
+# Specify the file path
+file_path = 'regex_sum_42.txt'
+
+# Open the file in read mode ('r')
+with open(file_path, 'r') as file:
+    # Read the contents of the file
+    file_contents = file.read()
+    
+print(file_contents)
+
+
+result_list = re.findall('[0-9]+', file_contents)
+int_list = [int(x) for x in result_list]
+
+result_sum = sum(int_list)
+print(result_sum)
